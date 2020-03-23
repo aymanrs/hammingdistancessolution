0da8f7803e98c2b950c679df2ba8628c06e33ccc
aymanrs <54403956+aymanrs@users.noreply.github.com>
2020-03-23 19:14:56 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+int totalHammingDistance(vector<int>& nums) {
+        int i;
+        unsigned long long total = 0;        
+        int bits[31] = {0};
+        for(int const& n : nums){
+            for(i = 0;i < 31;i++){
+                if(n & (1 << i)) bits[i]++;
+            }
+        }
+        for(i = 0;i < 31;i++){
+            total += bits[i] * (nums.size() - bits[i]);
+        }
+        return total;
+}
